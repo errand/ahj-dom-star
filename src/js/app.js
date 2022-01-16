4c5e94a248be1cd91d8f063a187d2f4e24a3c3d7
Aleksandr Shatskikh <work@errand.ru>
2022-01-16 23:03:13 +0300

    initial: commit

---
+++ src/js/app.js
@@ -0,0 +1,33 @@
+import data from '../data.json';
+
+(() => {
+  const movies = data;
+  const container = document.querySelector('#table-container');
+  const table = document.createElement('table');
+  container.append(table);
+
+  table.insertAdjacentHTML('afterbegin', ''
+    + '<tr>'
+    + '<th data-click="id">id</th>'
+    + '<th data-click="title">title</th>'
+    + '<th data-click="year">year</th>'
+    + '<th data-click="imdb">imdb</th></tr>');
+
+  const buttons = table.querySelectorAll('th[data-click]');
+
+  movies.forEach(el => {
+    const imdb = (el.imdb).toFixed(2);
+    table.insertAdjacentHTML('beforeend', `
+    <tr data-id="${el.id}" data-title="${el.title}" data-year="${el.year}" data-imdb="${imdb}">
+      <td>${el.id}</td>
+      <td>${el.title}</td>
+      <td>(${el.year})</td>
+      <td>${imdb}</td>
+    </tr>`);
+  });
+
+  buttons.forEach(el => el.addEventListener('click', (ev) => {
+    console.log(el);
+    console.log(table.querySelectorAll('tr'));
+  }));
+})();
