8b73641013e489f43d1d3f7890787177e9089f0b
Aleksandr Shatskikh <work@errand.ru>
2022-01-17 19:35:51 +0300

    feat: storing array

--- src/js/app.js
+++ src/js/app.js
@@ -2,27 +2,29 @@ import data from '../data.json';
 
 (() => {
   const movies = [...data];
+  const immutabelMovies = [...data];
   const container = document.querySelector('#table-container');
   const table = document.createElement('table');
   container.append(table);
+  table.insertAdjacentHTML('afterbegin', ''
+    + '<thead>'
+    + '<tr>'
+    + '<th data-click="id">id</th>'
+    + '<th data-click="title">title</th>'
+    + '<th data-click="year">year</th>'
+    + '<th data-click="imdb">imdb</th></tr>'
+    + '</thead><tbody></tbody>');
+
+  const buttons = table.querySelectorAll('th[data-click]');
+  buttons.forEach(el => el.addEventListener('click', () => clickTh(el)));
 
   function buildTable(rows) {
-    table.querySelectorAll('tr').forEach(tr => tr.remove());
-
-    table.insertAdjacentHTML('afterbegin', ''
-      + '<tr>'
-      + '<th data-click="id">id</th>'
-      + '<th data-click="title">title</th>'
-      + '<th data-click="year">year</th>'
-      + '<th data-click="imdb">imdb</th></tr>');
-
-    const buttons = table.querySelectorAll('th[data-click]');
-    buttons.forEach(el => el.addEventListener('click', () => clickTh(el.dataset.click)));
+    table.querySelector('tbody').querySelectorAll('tr').forEach(tr => tr.remove());
 
     rows.forEach(el => {
       const imdb = (el.imdb).toFixed(2);
-      table.insertAdjacentHTML('beforeend', `
-    <tr data-id="${el.id}" data-title="${el.title}" data-year="${el.year}" data-imdb="${imdb}">
+      table.querySelector('tbody').insertAdjacentHTML('beforeend', `
+    <tr>
       <td>${el.id}</td>
       <td>${el.title}</td>
       <td>(${el.year})</td>
@@ -31,22 +33,19 @@ import data from '../data.json';
     });
   }
 
-  function clickTh(click) {
-    const sorted = movies.sort((a, b) => {
-      if (a.click < b.click) { return -1; }
-      if (a.click > b.click) { return 1; }
-      return 0;
-    });
-    buildTable(sorted);
-  }
-
-  function clickTh(key) {
-    const sorted = movies.sort((a, b) => {
-      if (a[key] < b[key]) { return -1; }
-      if (a[key] > b[key]) { return 1; }
-      return 0;
-    });
-    buildTable(sorted);
+  function clickTh(el) {
+    if (el.classList.contains('active')) {
+      buildTable(immutabelMovies);
+      el.classList.remove('active');
+    } else {
+      const sorted = movies.sort((a, b) => {
+        if (a[el.dataset.click] < b[el.dataset.click]) { return -1; }
+        if (a[el.dataset.click] > b[el.dataset.click]) { return 1; }
+        return 0;
+      });
+      buildTable(sorted);
+      el.classList.add('active');
+    }
   }
 
   buildTable(movies);
