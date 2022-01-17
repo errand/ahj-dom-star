3f6f7d02a5ab87164f8680453bd9751c91700651
Aleksandr Shatskikh <work@errand.ru>
2022-01-17 14:35:41 +0300

    feat: storing data in DOM

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import data from '../data.json';
 
 (() => {
-  const movies = data;
+  const movies = [...data];
   const container = document.querySelector('#table-container');
   const table = document.createElement('table');
   container.append(table);
@@ -12,8 +12,6 @@ import data from '../data.json';
     + '<th data-click="title">title</th>'
     + '<th data-click="year">year</th>'
     + '<th data-click="imdb">imdb</th></tr>');
-
-  const buttons = table.querySelectorAll('th[data-click]');
 
   movies.forEach(el => {
     const imdb = (el.imdb).toFixed(2);
@@ -26,15 +24,43 @@ import data from '../data.json';
     </tr>`);
   });
 
-  buttons.forEach(el => el.addEventListener('click', () => {
+  function tableBuilder(rows) {
+    table.querySelectorAll('tr').forEach(tr => tr.remove());
+
+    table.insertAdjacentHTML('afterbegin', ''
+      + '<tr>'
+      + '<th data-click="id">id</th>'
+      + '<th data-click="title">title</th>'
+      + '<th data-click="year">year</th>'
+      + '<th data-click="imdb">imdb</th></tr>');
+
+    const buttons = table.querySelectorAll('th[data-click]');
+    buttons.forEach(el => el.addEventListener('click', () => clickTh()));
+
+    rows.forEach(el => {
+      const imdb = (el.imdb).toFixed(2);
+      table.insertAdjacentHTML('beforeend', `
+    <tr data-id="${el.id}" data-title="${el.title}" data-year="${el.year}" data-imdb="${imdb}">
+      <td>${el.id}</td>
+      <td>${el.title}</td>
+      <td>(${el.year})</td>
+      <td>${imdb}</td>
+    </tr>`);
+    });
+  }
+
+  const buttons = table.querySelectorAll('th[data-click]');
+
+  buttons.forEach(el => el.addEventListener('click', () => clickTh()));
+
+  function clickTh() {
     const rows = [...table.querySelectorAll('tr')];
     rows.sort((a, b) => {
       if (a.dataset.click < b.dataset.click) { return -1; }
       if (a.dataset.click > b.dataset.click) { return 1; }
       return 0;
     });
-    rows.forEach(tr => {
-      table.insertAdjacentElement('beforeend', tr);
-    });
-  }));
+    console.log(rows);
+    tableBuilder(rows);
+  }
 })();
